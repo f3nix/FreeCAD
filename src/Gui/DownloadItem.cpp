a9033c9517ad39b270b01f1466faacb8969aaf2c
Mateusz Skowroński <skowri@gmail.com>
2015-12-23 06:18:28 +0100

    Qt::escape() is obsolete in Qt5. Use QString::toHtmlEscaped() instead.
    This change is Qt5 specific.

--- src/Gui/DownloadItem.cpp
+++ src/Gui/DownloadItem.cpp
@@ -184,7 +184,12 @@ void NetworkAccessManager::authenticationRequired(QNetworkReply *reply, QAuthent
     dialog.adjustSize();
 
     QString introMessage = tr("<qt>Enter username and password for \"%1\" at %2</qt>");
+#if QT_VERSION >= 0x050000
+    introMessage = introMessage.arg(reply->url().toString().toHtmlEscaped())
+            .arg(reply->url().toString().toHtmlEscaped());
+#else
     introMessage = introMessage.arg(Qt::escape(reply->url().toString())).arg(Qt::escape(reply->url().toString()));
+#endif
     passwordDialog.siteDescription->setText(introMessage);
     passwordDialog.siteDescription->setWordWrap(true);
 
@@ -206,7 +211,11 @@ void NetworkAccessManager::proxyAuthenticationRequired(const QNetworkProxy &prox
     dialog.adjustSize();
 
     QString introMessage = tr("<qt>Connect to proxy \"%1\" using:</qt>");
+#if QT_VERSION >= 0x050000
+    introMessage = introMessage.arg(proxy.hostName().toHtmlEscaped());
+#else
     introMessage = introMessage.arg(Qt::escape(proxy.hostName()));
+#endif
     proxyDialog.siteDescription->setText(introMessage);
     proxyDialog.siteDescription->setWordWrap(true);
 
